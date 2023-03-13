6bbb1e5c87cb00497e5e43ce385edfc3b189715f
Luis German Vargas Hernandez <gercool92@gmail.com>
2023-03-12 18:34:02 -0600

    Subo cambios del modulo de maestros en el proyecto

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import forms
 from flask import jsonify
 from config import DevelopmentConfig
 from flask_wtf.csrf import CSRFProtect
-from models import db, Alumnos
+from models import db, Alumnos, Maestros
+from db import get_connection
+
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
@@ -75,11 +77,119 @@ def eliminar():
         db.session.commit()
         return redirect(url_for('ABCompleto'))
         
-    return render_template('eliminar.html', form = form)
+    return render_template('eliminar.html', form = form, alum1 = alum1)
+
+'''******************Seccion de maestros ***********************************'''
+@app.route('/ABCompletoTeachers', methods=['GET','POST'])
+def ABCompletoTeachers():
+    form = forms.TeachForm(request.form)
+    try:
+        connection = get_connection()
+        with connection.cursor() as cursor:
+            cursor.execute('call consulta_maestros()')
+            teacher = cursor.fetchall()
+        connection.close()
+    except Exception as ex:
+            print('ERROR')
+    
+    return render_template('ABCompletoTeachers.html', form = form, teacher = teacher)
+
+@app.route('/formularioTeachers', methods=['GET','POST'])
+def formularioTeachers():
+    form = forms.TeachForm(request.form)
+    if request.method == 'POST':
+        nombre = form.nombre.data
+        apellidos = form.apellidos.data
+        email = form.email.data 
+        try:
+            connection = get_connection()
+            with connection.cursor() as cursor:
+                cursor.execute('call agrega_maestro(%s,%s,%s)', (nombre,apellidos,email))
+            connection.commit()
+            connection.close()
+        except Exception as ex:
+            print('ERROR')
+        return redirect(url_for('ABCompletoTeachers'))
         
+    return render_template('Maestros.html', form = form)
+
+@app.route('/modificarTeachers', methods=['GET', 'POST'])
+def modificarTeachers():
+    form = forms.TeachForm(request.form)
+    if request.method == 'GET':
+        id = request.args.get('id')
+
+        try:
+            connection = get_connection()
+            with connection.cursor() as cursor:
+                cursor.execute('call consulta_maestro('+id+')')
+                teach1 = cursor.fetchall()
+                t = teach1[0]
+            connection.close()
+        except Exception as ex:
+            print('ERROR')
+
+        form.id.data = t[0]
+        form.nombre.data = t[1]
+        form.apellidos.data = t[2]
+        form.email.data = t[3]
+    
+    if request.method == 'POST':
+        id = form.id.data
+        nombre = form.nombre.data
+        apellidos = form.apellidos.data
+        email = form.email.data
+        try:
+            connection = get_connection()
+            with connection.cursor() as cursor:
+                cursor.execute('call modificar_maestro(%s,%s,%s,%s)', (id, nombre, apellidos, email)) 
+            connection.commit()
+            connection.close()
+        except Exception as ex:
+            print(ex)
+
+        return redirect(url_for('ABCompletoTeachers'))
+    return render_template('modificarTeachers.html', form=form)
+
+@app.route('/eliminarTeachers', methods=['GET','POST'])
+def eliminarTeachers():
+    form = forms.TeachForm(request.form)
+    if request.method == 'GET':
+        id = request.args.get('id')
+
+        try:
+            connection = get_connection()
+            with connection.cursor() as cursor:
+                cursor.execute('call consulta_maestro('+id+')')
+                teach1 = cursor.fetchall()
+                t = teach1[0]
+            connection.close()
+        except Exception as ex:
+            print('ERROR')
+        
+        form.id.data = request.args.get('id')
+        form.nombre.data = t[1]
+        form.apellidos.data = t[2]
+        form.email.data = t[3]
+
+    if request.method == 'POST':
+        id2 = form.id.data
+        try:
+            connection = get_connection()
+            with connection.cursor() as cursor:
+                cursor.execute('call eliminar_maestro(%s)', (id2,)) 
+            connection.commit()
+            connection.close()
+        except Exception as ex:
+            print('ERROR')
+
+        return redirect(url_for('ABCompletoTeachers'))
+        
+    return render_template('eliminarTeachers.html', form = form)
+
 if __name__ == '__main__':
     csrf.init_app(app)
     db.init_app(app)
     with app.app_context():
         db.create_all()
-    app.run(port=5000)
+    app.run(port=3000)
--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\varga\Desktop\git_prueba\Proyecto_BD\env
+set VIRTUAL_ENV=C:\Users\varga\Desktop\git_prueba\IDGS802-Proyecto_BD\env
 
 if not defined PROMPT set PROMPT=$P$G
 
